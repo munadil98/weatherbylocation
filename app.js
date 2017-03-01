c8f8d436b47e6ac1a22ad0670562c17ecd7686fd
Munadil Fahad <munadil98@gmail.com>
2017-03-02 00:42:18 +0600

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function showPosition(position) {
     // alert(ypos);
 
     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
-    +latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
+    +latlon+"&zoom=16&size=400x300&sensor=false&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 
 
